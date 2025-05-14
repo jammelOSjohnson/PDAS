a06b61142568a811a9ae69fdbf013e67cf2112d9
Jammel Johnson <jammel.johnson@myguardiangroup.com>
2025-05-14 17:52:44 -0500

    Refactor App component to include a hero section and enhance ProfileCard with descriptions for each assistant

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const ProfileCard = ({ title, handle, image }) => {
+const ProfileCard = ({ title, handle, image, description }) => {
   return (
     <div className="card">
       <div className="card-image">
@@ -13,6 +13,7 @@ const ProfileCard = ({ title, handle, image }) => {
           <p className="title is-4">{title}</p>
           <p className="subtitle is-6">{handle}</p>
         </div>
+        <div className="content">{description}</div>
       </div>
     </div>
   );
