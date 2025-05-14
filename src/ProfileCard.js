78992e2d7ce012cabc6f8a0fe0c38a5754504618
Jammel Johnson <jammel.johnson@myguardiangroup.com>
2025-05-14 17:07:52 -0500

    Enhance ProfileCard component with improved layout and styling

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -2,10 +2,18 @@ import React from "react";
 
 const ProfileCard = ({ title, handle, image }) => {
   return (
-    <div>
-      <img src={image} alt={title} />
-      <div>Title is {title}</div>
-      <div>Handle is {handle}</div>
+    <div className="card">
+      <div className="card-image">
+        <figure class="image is-4by3">
+          <img src={image} alt={title} className="card-image" />
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="media-content">
+          <p className="title is-4">{title}</p>
+          <p className="subtitle is-6">{handle}</p>
+        </div>
+      </div>
     </div>
   );
 };
